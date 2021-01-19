2822a388b580ad4c28c694c9e55f5d539d020e25
cmalven <chris@malven.co>
2021-01-19 16:59:34 -0600

    Update THREE image example frag shader to include a better setup for manipulating pixels.

--- src/modules/shaders/threeImageExampleFrag.glsl
+++ src/modules/shaders/threeImageExampleFrag.glsl
@@ -8,5 +8,15 @@ varying vec3 vPosition;
 // uniform vec3 cameraPosition;
 
 void main() {
-  gl_FragColor = texture2D(imageTexture, vUv);
+  float u = vUv.x;
+  float v = vUv.y;
+  vec2 newUv = vec2(u, v);
+
+  float r = texture2D(imageTexture, newUv).r;
+  float g = texture2D(imageTexture, newUv).g;
+  float b = texture2D(imageTexture, newUv).b;
+
+  vec3 texture = vec3(r, g, b);
+
+  gl_FragColor = vec4(texture, 1.0);
 }
