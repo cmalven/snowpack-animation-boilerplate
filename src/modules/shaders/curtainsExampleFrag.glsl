556b88ea2f324d3a44372fa4cfb4c19d4a5a52cc
cmalven <chris@malven.co>
2021-01-26 13:52:54 -0600

    Update Curtains example frag shader for more obvious explanation for how to modify UV and RGB.

--- src/modules/shaders/curtainsExampleFrag.glsl
+++ src/modules/shaders/curtainsExampleFrag.glsl
@@ -8,11 +8,21 @@ uniform float uTime;
 uniform sampler2D uSampler0;
 
 void main() {
-  vec2 textureCoord = vTextureCoord;
+    float u = vTextureCoord.x;
+    float v = vTextureCoord.y;
+    vec2 newUv = vec2(u, v);
 
-  // Distort texture coordinates (as an example)
-  textureCoord.x += sin(textureCoord.y * 25.0) * cos(textureCoord.x * 25.0) * (cos(uTime / 50.0)) / 25.0;
+    // Distort texture
+    newUv.x += sin(newUv.y * 25.0) * cos(newUv.x * 25.0) * (cos(uTime / 50.0)) / 25.0;
 
-  // map our texture with the texture matrix coords
-  gl_FragColor = texture2D(uSampler0, textureCoord);
+    // Add highlight based on z position
+    float zHighlight = vVertexPosition.z * 1.0;
+
+    float r = texture2D(uSampler0, newUv).r + zHighlight;
+    float g = texture2D(uSampler0, newUv).g + zHighlight;
+    float b = texture2D(uSampler0, newUv).b + zHighlight;
+
+    vec3 texture = vec3(r, g, b);
+
+    gl_FragColor = vec4(texture, 1.0);
 }
